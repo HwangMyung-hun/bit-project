e4d00947a56b2d4b19d611eadd1dbb0cc804b2c6
ujbj84 <ujbj84@naver.com>
2015-06-11 11:19:19 +0900

    dz

--- ddalki/src/main/java/net/totalCloud/DDalki/web/UserController.java
+++ ddalki/src/main/java/net/totalCloud/DDalki/web/UserController.java
@@ -1,5 +1,14 @@
 package net.totalCloud.DDalki.web;
 
+import java.util.HashMap;
+
+import net.totalCloud.DDalki.dao.UserDao;
+import net.totalCloud.DDalki.domain.UserVo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,4 +16,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/ddalki")
 public class UserController {
   
+  @Autowired
+  UserDao userDao;
+  
+  @RequestMapping("/add")
+  public ResponseEntity<String> add(UserVo user) throws Exception {
+    userDao.insert(user);
+    
+    HashMap<String,Object> responseData = new HashMap<String,Object>();
+    responseData.put("status", "success");
+    
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "text/plain;charset=UTF-8");
+    headers.add("Access-Control-Allow-Origin", "*");
+    
+    return new ResponseEntity<String>(
+                new Gson().toJson(responseData),
+                headers,
+                HttpStatus.OK);
+  }
 }
