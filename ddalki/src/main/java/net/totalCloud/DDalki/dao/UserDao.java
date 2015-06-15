90be988f5dab65044ee5cbbe59cb89563c3948c2
ujbj84 <ujbj84@naver.com>
2015-06-15 19:35:48 +0900

    .

--- ddalki/src/main/java/net/totalCloud/DDalki/dao/UserDao.java
+++ ddalki/src/main/java/net/totalCloud/DDalki/dao/UserDao.java
@@ -1,10 +1,15 @@
 package net.totalCloud.DDalki.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.totalCloud.DDalki.domain.UserVo;
 
 
 public interface UserDao {
-  int insert(UserVo user);
+  void insert(UserVo user);
+
+  String duplication(String user);
   
-  
+  UserVo selectOne(HashMap<String, Object> sqlParams);
 }
