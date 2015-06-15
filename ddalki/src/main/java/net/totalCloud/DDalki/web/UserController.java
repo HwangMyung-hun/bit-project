fb2ba29d6c729095c8be6726b6aab19ee5c9886f
ujbj84 <ujbj84@naver.com>
2015-06-15 19:36:15 +0900

    .

--- ddalki/src/main/java/net/totalCloud/DDalki/web/UserController.java
+++ ddalki/src/main/java/net/totalCloud/DDalki/web/UserController.java
@@ -2,6 +2,10 @@ package net.totalCloud.DDalki.web;
 
 import java.util.HashMap;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import net.totalCloud.DDalki.dao.UserDao;
 import net.totalCloud.DDalki.domain.UserVo;
 
@@ -11,28 +15,62 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
-@RequestMapping("/ddalki")
+import com.google.gson.Gson;
+
+@Controller("/ddalki")
+@RequestMapping("/cloud")
 public class UserController {
   
   @Autowired
   UserDao userDao;
   
   @RequestMapping("/add")
-  public ResponseEntity<String> add(UserVo user) throws Exception {
+  public Object add(UserVo user) throws Exception {
     userDao.insert(user);
     
     HashMap<String,Object> responseData = new HashMap<String,Object>();
     responseData.put("status", "success");
     
-    HttpHeaders headers = new HttpHeaders();
-    headers.add("Content-Type", "text/plain;charset=UTF-8");
-    headers.add("Access-Control-Allow-Origin", "*");
+    return responseData;
+  }
+  
+  @RequestMapping("/duplication")
+  public Object duplication(String email) throws Exception {
+    HashMap<String,Object> responseData = new HashMap<String,Object>();
+    responseData.put("status", "success");
+    if (userDao.duplication(email) == null) {
+      responseData.put("duplication", "ok");      
+    } else {
+      responseData.put("duplication", "no");
+    }
     
-    return new ResponseEntity<String>(
-                new Gson().toJson(responseData),
-                headers,
-                HttpStatus.OK);
+    return responseData;
+  }
+  
+  @RequestMapping(value="/login",method=RequestMethod.POST)
+  public Object login(String loginemail, String loginpwd, HttpSession session, HttpServletResponse response) throws Exception{
+    
+    HashMap<String,Object> sqlParams = new HashMap<String,Object>();
+    sqlParams.put("email", loginemail);
+    sqlParams.put("pwd", loginpwd);
+    
+    HashMap<String,Object> responseData = new HashMap<String,Object>();
+    responseData.put("status", "success");
+    
+    UserVo user = userDao.selectOne(sqlParams);
+    
+    if (user == null ) {
+      responseData.put("idcheck", "no");
+      session.invalidate();
+    } else {
+      responseData.put("idcheck", "yes");
+      responseData.put("data", userDao.selectOne(sqlParams));
+      session.setAttribute("user", user);
+      System.out.println(((UserVo)session.getAttribute("user")).getName());
+    }
+    
+    return responseData;
   }
 }
