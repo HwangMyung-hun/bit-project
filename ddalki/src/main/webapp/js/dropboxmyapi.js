2cb4824e46fd1bfd252c2e5958649ab3c68b1590
HwangMyung-hun <pomu122@gmail.com>
2015-07-13 18:16:00 +0900

    /.

--- ddalki/src/main/webapp/js/dropboxmyapi.js
+++ ddalki/src/main/webapp/js/dropboxmyapi.js
@@ -4,9 +4,78 @@ var alldirandfile = [];
 client.authenticate({ interactive: false }, function (error, client) {
   if (error) {
       alert('Error: ' + error);
+  } else {
+	  if(client._credentials != null) {
+      	var ip = 'localhost:9999/ddalki';
+        var directoryLocation = '/cloud';
+	    $.ajax('http://' + ip + directoryLocation + '/hasCloud.do', {
+	        method: 'POST',
+	        dataType: 'json',
+	        data: {
+	          email: "aa",
+	          cloudtype: "dropbox",
+	          cloudid: "allow"
+	        },
+	        success: function(result) {
+	        	dropboxactive();
+	        	if (alldirandfile.length == 0 && client.isAuthenticated()) {
+        		  function againlist(path) {
+        		    client.readdir(path, function (error, results, now, under) {
+        		      if (error) {
+        		        console.log(error);   
+        		      }
+        		      alldirandfile.push(now);
+        		      for(var i = 0; i < under.length; i++) {
+        		        if(under[i].mimeType == "inode/directory") againlist(under[i].path);
+        		      }
+        		    });
+        		  }
+        		  againlist('/');
+        		}
+	        },
+	        error: function(xhr, textStatus, errorThrown) {
+	          alert('자걱.\n' + 
+	              '잠시 후 다시 시도하세요.\n' +
+	          '계속 창이 뜬다면, 관리자에 문의하세요.(사내번호:1112)');
+	        }
+	      });
+	  } else {
+		  dropboxunactive();
+	  }
   }
 });
- 
+
+function dropboxactive() {
+	//존재하면서 활성화
+}
+
+function dropboxunactive() {
+	var ip = 'localhost:9999/ddalki';
+	  var directoryLocation = '/cloud';
+	  $.ajax('http://' + ip + directoryLocation + '/hasCloud.do', {
+	    method: 'POST',
+	    dataType: 'json',
+	    data: {
+	      email: "aa",
+	      cloudtype: "dropbox",
+	      cloudid: "allow"
+	    },
+	    success: function(result) {
+	    	if(result.cloud == "exist") {
+	    		// 있으면서 비활성화
+	    	} else {
+	    		// 존재조차 안함
+	    	}
+	    },
+	    error: function(xhr, textStatus, errorThrown) {
+	      alert('자걱.\n' + 
+	          '잠시 후 다시 시도하세요.\n' +
+	      '계속 창이 뜬다면, 관리자에 문의하세요.(사내번호:1112)');
+	    }
+	  });
+}
+
+
 if (client.isAuthenticated()) {
   function againlist(path) {
     client.readdir(path, function (error, results, now, under) {
@@ -14,8 +83,10 @@ if (client.isAuthenticated()) {
         console.log(error);   
       }
       alldirandfile.push(now);
-      for(var i = 0; i < under.length; i++) {
-        if(under[i].mimeType == "inode/directory") againlist(under[i].path);
+      if(under != undefined) {
+    	  for(var i = 0; i < under.length; i++) {
+    		  if(under[i].mimeType == "inode/directory") againlist(under[i].path);
+    	  }
       }
     });
   }
@@ -90,8 +161,14 @@ var droptarget = '';
 function dropboxfiletable(id) {
 	//id가 해쉬값, 디렉토리의 배열값(파일, 폴더)
 	$('#dropupbtn').remove();
-	if(id != "/") droptarget = id;
+	var thumnailobj;
+	var thumnailtag;
+	if(id != "/") {
+		thumnailobj = thumnailFind(dropArrayFind(id)[0].path, "path");
+		droptarget = id;
+	}
 	else if(id == "/"){
+		thumnailobj = thumnailFind("", "path");
 		id = droprootFind()[0].contentHash;
 		droptarget = "/";
 	}
@@ -104,9 +181,19 @@ function dropboxfiletable(id) {
 			title = tablevalue[i].path.split("/");
 			time = tablevalue[i].modified.split(" ");
 			time = time[3] + "-" + months[time[2]] + "-" + time[1];
+			if(thumnailobj.length != 0) {
+				if(thumnailFind(title[title.length - 1],"name")[0].thumnail == "undefined") {
+					thumnailtag = "";
+				} else {
+					thumnailtag = "<img src='"+ thumnailFind(title[title.length - 1] 
+					,"name")[0].thumnail +"' id='image1'>";
+				}
+			} else {
+				thumnailtag = "";
+			}
 			$("#tbody").append("<tr><td><input type='checkbox' name='" 
 					                + tablevalue[i].path + "'></td>"
-                                    + "<td>"+ title[title.length - 1] +"</td>"
+                                    + "<td>"+ title[title.length - 1] + thumnailtag +"</td>"
                                     + "<td></td>"
                                     + "<td>"+ time +"</td>"
                                     + "<td class='center'>"+ tablevalue[i].size +"</td>"
@@ -123,6 +210,71 @@ function dropboxfiletable(id) {
                     + "<td class='center'></td>"
                     + "<td class='center'>folder</td></tr>");
 		}
+	}
+	etcthumnail();
+}
+
+function etcthumnail() {
+	var td = $('#tbody tr td:nth-child(6)');
+	for (var i = 0; i < td.length; i++ ) {
+		if(td[i].innerText != "jpg" && td[i].innerText != "png") {
+			td[i].parentElement.childNodes[1].innerHTML += imghtmltag(td[i].innerText);
+		}
+	}
+}
+
+function imghtmltag(type) {
+	switch (type) {
+	case "ppt":
+		return "<img src='../img/fileicon_ppt.png' id='image1'>";
+		break;
+	case "pptx":
+		return "<img src='../img/fileicon_ppt.png' id='image1'>";
+		break;
+	case "folder":
+		return "<img src='../img/fileicon_folder.png' id='image1'>";
+		break;
+	case "hwp":
+		return "<img src='../img/fileicon_doc.png' id='image1'>";
+		break;
+	case "doc":
+		return "<img src='../img/fileicon_doc.png' id='image1'>";
+		break;
+	case "avi":
+		return "<img src='../img/fileicon_avi.png' id='image1'>";
+		break;
+	case "html":
+		return "<img src='../img/fileicon_html.png' id='image1'>";
+		break;
+	case "mp3":
+		return "<img src='../img/fileicon_mp3.png' id='image1'>";
+		break;
+	case "pdf":
+		return "<img src='../img/fileicon_pdf.png' id='image1'>";
+		break;
+	case "text":
+		return "<img src='../img/fileicon_text.png' id='image1'>";
+		break;
+	case "wav":
+		return "<img src='../img/fileicon_wav.png' id='image1'>";
+		break;
+	case "wma":
+		return "<img src='../img/fileicon_wma.png' id='image1'>";
+		break;
+	case "xls":
+		return "<img src='../img/fileicon_xls.png' id='image1'>";
+		break;
+	case "zip":
+		return "<img src='../img/fileicon_zip.png' id='image1'>";
+		break;
+	case "gif":
+		return "<img src='../img/fileicon_gif.png' id='image1'>";
+		break;
+	case "bmp":
+		return "<img src='../img/fileicon_bmp.png' id='image1'>";
+		break;
+	default:
+		return "<img src='../img/quest.jpg' id='image1'>";
 	}
 }
 
@@ -304,41 +456,39 @@ $('.left img:nth-child(6)').click(function() {
 	}
 });
 
-var dropPreTitle;
 $('.left img:nth-child(7)').click(function() {
 	console.log(dropboxallow);
 	if(dropboxallow) {
-	  var checkreset = $('#tbody input');
-	  for(var i = 0; i < checkreset.length; i++) {
-		  checkreset[i].checked = false;
-	  }
-	  alert("checkbox를 선택하시오!");
-      $('#tbody input').click(function(e) {
-    	  var titleTd = e.target.parentNode.parentNode.childNodes[1];
-    	  var mimeType = e.target.parentNode.parentNode.childNodes[5].innerText;
-    	  if(titleTd.innerText != "등록취소") dropPreTitle = titleTd.innerText;
-    	  if(e.target.checked) {
-    		  console.log(dropPreTitle);
-    		  titleTd.innerText = '';
-    		  titleTd.innerHTML = '<span><input id="dropnewtitle" type="text"/><button id="dropin">등록</button>' 
-    			  + '<button id="dropgetoutofhere">취소</button></span>';
-    		  $('#dropin').click(function() {
-    			  if(mimeType == "" || mimeType == "folder") {
-    				  mimeType = "";
-    				  dropRenameFile($('#dropnewtitle').val() + mimeType);
-    			  } else {
-    				  dropRenameFile($('#dropnewtitle').val() + "." + mimeType);
-    			  }
-    		  });
-    		  $('#dropgetoutofhere').click(function() {
-    			  titleTd.innerHTML = dropPreTitle;
-    			  titleTd.innerText = dropPreTitle;
-    			  e.target.checked = false;
-    		  });
-    	  } else {
-    		  titleTd.innerText = dropPreTitle;
-    	  }
-      });
+      var tr = $("#tbody input");
+      var name = null;
+      var j = 0;
+	  for (var i = 0; i < tr.length; i++) {
+		  if(tr[i].checked && j == 0) {
+			  j++;
+			  name = tr[i];
+		  }
+		  if(tr[i].checked && j != 0 && j != 1) {
+			  tr[i].checked = false;
+		  }
+      }
+	  var titleTd = $(name.parentNode.parentNode.childNodes[1])[0];
+	  var mimeType = $(name.parentNode.parentNode.childNodes[5])[0].innerText;
+	  if(name.checked) {
+		  titleTd.innerText = '';
+		  titleTd.innerHTML = '<span><input id="dropnewtitle" type="text"/><button id="dropin">등록</button>' 
+			  + '<button id="dropgetoutofhere">취소</button></span>';
+		  $('#dropin').click(function() {
+			  if(mimeType == "" || mimeType == "folder") {
+				  mimeType = "";
+				  dropRenameFile($('#dropnewtitle').val() + mimeType);
+			  } else {
+				  dropRenameFile($('#dropnewtitle').val() + "." + mimeType);
+			  }
+		  });
+		  $('#dropgetoutofhere').click(function() {
+			  $('#' + droptarget).trigger("click");
+		  });
+	  } 
 	}
 });
 
@@ -351,6 +501,7 @@ function dropRenameFile(name) {
 	client.move(input[0].name, path ,function (error, result) {
 		if(!error) {
 			droprefresh(result);
+			console.log(result);
 		} else {
 			alert("같은 이름이 존재합니다");
 		}
@@ -383,3 +534,39 @@ function droprefresh(result) {
 $('.btn-social').click(function() {
 	$('#side-menu').metisMenu();
 });
+
+var thumnailarray = [];
+
+$('.btn-dropbox').click(function() {
+	for(var i = 0; i < alldirandfile.length; i++) {
+		for(var j = 0; j < alldirandfile[i]._json.contents.length; j++) {
+			var thumnail = {};
+			if (alldirandfile[i]._json.contents[j].mime_type != undefined) {
+				var img = alldirandfile[i]._json.contents[j].mime_type.split("/")[0];
+				if(img == "image") {
+					thumnail.thumnail = client.thumbnailUrl(alldirandfile[i]._json.contents[j].path, {});
+					thumnail.mime = alldirandfile[i]._json.contents[j].mime_type;
+					thumnail.path = alldirandfile[i]._json.contents[j].path;
+					thumnail.name = alldirandfile[i]._json.contents[j].path.
+					split("/")[alldirandfile[i]._json.contents[j].
+					           path.split("/").length - 1];
+					thumnail.parent_dir_path = thumnail.path.split("/" + thumnail.name)[0];
+					thumnailarray.push(thumnail);
+				}
+			}
+		}
+	}
+});
+
+function thumnailFind(path , str) {
+	if(str == "path") {
+		return thumnailarray.filter(function( obj ) {
+			return obj.parent_dir_path == path;
+		});
+	} else {
+		return thumnailarray.filter(function( obj ) {
+			return obj.name == path;
+		});
+	}
+}
+
